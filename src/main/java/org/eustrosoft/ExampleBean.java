a94fdfbb0ccab0beb1143145db5822cb1d33db30
Pavel <pavel-seleznev@inbox.ru>
2020-02-16 03:28:18 -0600

    EJB + pgSQL + 3 pages with entities

--- src/main/java/org/eustrosoft/ExampleBean.java
+++ src/main/java/org/eustrosoft/ExampleBean.java
@@ -1,16 +1,21 @@
 package org.eustrosoft;
 
-import javax.enterprise.context.SessionScoped;
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 import java.io.Serializable;
+import java.util.List;
 
 @Named
-@SessionScoped
+@RequestScoped
 public class ExampleBean implements Serializable {
-    private String welcome = "Hello";
     private String login;
     private String password;
     private boolean logged;
+    private boolean createSuccess;
+
+    @EJB
+    private ExampleEJB exampleEJB;
 
     public void doLogin(){
         logged = true;
@@ -40,11 +45,31 @@ public class ExampleBean implements Serializable {
         this.password = password;
     }
 
-    public String getWelcome() {
-        return welcome;
+    public boolean isCreateSuccess() {
+        return createSuccess;
     }
 
-    public void setWelcome(String welcome) {
-        this.welcome = welcome;
+    public void setCreateSuccess(boolean createSuccess) {
+        this.createSuccess = createSuccess;
+    }
+
+    public ExampleEJB getExampleEJB() {
+        return exampleEJB;
+    }
+
+    public void setExampleEJB(ExampleEJB exampleEJB) {
+        this.exampleEJB = exampleEJB;
+    }
+
+    public void checkPassword(){
+        logged = exampleEJB.checkPassword(login, password);
+    }
+
+    public void createUser(){
+        createSuccess = exampleEJB.createUser(login, password);
+    }
+
+    public List<UserEntity> getAllUsers(){
+        return exampleEJB.getAllUsers();
     }
 }
